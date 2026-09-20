586822e66c88d63e248bd4b67fed18c907b9488b
Jasmeet Kaur <jasmeetk@nedbank.co.za>
2026-09-20 20:01:41 +0530

    Fixes

--- test_request.py
+++ test_request.py
@@ -1,6 +1,6 @@
 import requests, json
 
-with open("sample_request.json") as f:
+with open("sample_request2.json") as f:
     payload = json.load(f)
 
 response = requests.post("http://127.0.0.1:5000/predict", json=payload)
